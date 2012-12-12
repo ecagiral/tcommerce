5a764885cec085d6ba5c15d0e60e67aa313e903e
Engin Guller <bisanthe@gmail.com>
2012-12-12 23:28:34 +0200

    build file

---
+++ build.sh
@@ -0,0 +1,10 @@
+$PLAY_HOME=/home/tcommerce/play-1.2.5
+$PLAY=$PLAY_HOME/play
+$TCOMMERCE_SRC=/home/tcommerce/tcommerce
+$TCOMMERCE_PROD=/home/tcommerce/tcommerce_prod
+
+$PLAY stop $TCOMMERCE_HOME
+git pull
+rm -Rf $TCOMMERCE_PROD
+mkdir $TCOMMERCE_PROD
+cp -R $TCOMMERCE_SRC/* $TCOMMERCE_PROD/
