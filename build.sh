220f10f49dab0b76edc72b9d7507e99ecc6be2ef
Engin Guller <bisanthe@gmail.com>
2012-12-13 01:00:58 +0200

    build file

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ PLAY=$PLAY_HOME/play
 TCOMMERCE_SRC=/home/tcommerce/tcommerce
 TCOMMERCE_PROD=/home/tcommerce/tcommerce_prod
 
-$PLAY stop $TCOMMERCE_HOME
+$PLAY stop $TCOMMERCE_PROD
 git pull
 rm -Rf $TCOMMERCE_PROD
 mkdir $TCOMMERCE_PROD
 cp -R $TCOMMERCE_SRC/* $TCOMMERCE_PROD/
+rm $TCOMMERCE_PROD/README.md
+rm $TCOMMERCE_PROD/build.sh
+$PLAY start 9010 $TCOMMERCE_PROD
