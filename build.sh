819c16f4d19b02c8760108defc3fd941731b423a
Engin Guller <bisanthe@gmail.com>
2012-12-13 01:29:34 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ PLAY=$PLAY_HOME/play
 TCOMMERCE_SRC=/home/tcommerce/tcommerce
 TCOMMERCE_PROD=/home/tcommerce/tcommerce_prod
 
+git pull
+
 cd $TCOMMERCE_PROD
 $PLAY stop $TCOMMERCE_PROD
-git pull
+
 rm -Rf $TCOMMERCE_PROD
 mkdir $TCOMMERCE_PROD
 cp -R $TCOMMERCE_SRC/* $TCOMMERCE_PROD/
@@ -13,4 +15,5 @@ rm $TCOMMERCE_PROD/README.md
 rm $TCOMMERCE_PROD/build.sh
 sed 's/application.mode=dev/application.mode=prod/' $TCOMMERCE_PROD/conf/application.conf > $TCOMMERCE_PROD/conf/application.conf
 cd $TCOMMERCE_PROD
+
 $PLAY start -Dhttp.port=9010
