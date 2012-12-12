252dea55363d92abfe1993545f637b917b87b6ea
Engin Guller <bisanthe@gmail.com>
2012-12-12 23:31:51 +0200

    update build file

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-$PLAY_HOME=/home/tcommerce/play-1.2.5
-$PLAY=$PLAY_HOME/play
-$TCOMMERCE_SRC=/home/tcommerce/tcommerce
-$TCOMMERCE_PROD=/home/tcommerce/tcommerce_prod
+PLAY_HOME=/home/tcommerce/play-1.2.5
+PLAY=$PLAY_HOME/play
+TCOMMERCE_SRC=/home/tcommerce/tcommerce
+TCOMMERCE_PROD=/home/tcommerce/tcommerce_prod
 
 $PLAY stop $TCOMMERCE_HOME
 git pull
